7e4cce9bf35710a7835560801bd03a22e8260db9
tomershoham10 <tomershoham10@gmail.com>
2024-09-25 21:12:10 +0300

    feat: stepsSections

--- src/recipes/model.ts
+++ src/recipes/model.ts
@@ -1,37 +1,46 @@
 import mongoose, { Schema } from "mongoose";
 
 enum RecipeCategories {
-  ITALIAN = "italian",
-  ASAIN = "asain",
-  INDIAN = "indian",
-  VEGAN = "vegan",
-  SEAFOOD = "seafood",
-  SALAD = "salad",
-  DINNER = "dinner",
-  DESSERT = "desset",
+  ITALIAN = "איטלקי",
+  ASAIN = "אסייתי",
+  INDIAN = "הודי",
+  VEGAN = "טבעוני",
+  VEGETERIAN = "צמחוני",
+  SEAFOOD = "דגים",
+  SALAD = "סלט",
+  DINNER = "ארוחת ערב",
+  DESSERT = "קינוח",
+  COCKTAIL = "קוקטייל",
+  SOUP = "מרק",
+  BAKING = "אפייה",
 }
 
-enum difficultyLevels {
-  EASY = "easy",
-  MEDIUM = "medium",
-  ADVANCED = "advanced",
+enum DifficultyLevels {
+  EASY = "קל",
+  MEDIUM = "בינוני",
+  ADVANCED = "למתקדמים",
 }
 
 const recipesSchema: Schema = new Schema<RecipeType>(
   {
     name: { type: String, required: true, unique: true, minlength: 3 },
+
     description: { type: String, required: true, minlength: 5 },
-    img: { type: String, required: true, minlength: 5 },
+
+    picture: { type: String, required: true, minlength: 5 },
+
     categories: [{
       type: String,
       enum: Object.values(RecipeCategories),
       required: true,
     }],
+
     difficultyLevel: {
       type: String,
-      enum: Object.values(difficultyLevels),
+      enum: Object.values(DifficultyLevels),
       required: true,
     },
+
     ingredientsSections: [
       {
         header: { type: String, required: true },
@@ -44,6 +53,19 @@ const recipesSchema: Schema = new Schema<RecipeType>(
             },
             quantity: { type: Number, required: true },
             unit: { type: String, required: true },
+            index: { type: Number, required: true }
+          },
+        ],
+        index: { type: Number, required: true }
+      },
+    ],
+
+    stepsSections: [
+      {
+        header: { type: String, required: true },
+        quantifiedIngredients: [
+          {
+            quantity: { type: String, required: true },
             index: { type: Number, required: true }
           },
         ],
